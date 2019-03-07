4181cf446b392bcc4806e706373a5b88e6db3ffb
bmq4 <bmq4@cornell.edu>
2019-03-07 18:27:19 -0500

    Commented two_wheel.py

--- lab3/two_wheel.py
+++ lab3/two_wheel.py
@@ -3,7 +3,13 @@
 
 import RPi.GPIO as GPIO
 
+# Function for setting direction of a servo
+# PWM_number is a pointer to a GPIO.PWM object
+# Direction is a string equal to one of: - "stop"
+#                                        - "clockwise"
+#                                        - "counter-clockwise"
 def set_direction(PWM_number, direction):
+    # Set freq and dc based on desired direction
     if direction = "stop":
         freq = 50
         dc = 0
@@ -13,25 +19,28 @@ def set_direction(PWM_number, direction):
     if direction = "counter-clockwise":
         freq = 46.083
         dc = 7.834
+    # Update servo with new values
     PWM_number.ChangeFrequency(freq)
     PWM_number.ChangeDutyCycle(dc)
 
+# Set up GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(26,GPIO.OUT)
-
-left_servo = GPIO.PWM(13, 50)
-right_servo = GPIO.PWM(26, 50)
-
-
 GPIO.setup(17,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(19,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Initialize servo objects
+left_servo = GPIO.PWM(13, 50)
+right_servo = GPIO.PWM(26, 50)
+
+# Left vs Right servo selected
 left = False
 
+# Interrupt function calls
 def stop(channel):
     if left:
         set_direction(left_servo, "stop")
@@ -50,11 +59,13 @@ def counter_clockwise(channel):
 def swap_servo(channel):
     left = not left
 
+# Attach interrupts to falling edges
 GPIO.add_event_detect(17, GPIO.FALLING, callback=counter_clockwise, bouncetime=200)
 GPIO.add_event_detect(19, GPIO.FALLING, callback=swap_servo, bouncetime=200)
 GPIO.add_event_detect(22, GPIO.FALLING, callback=clockwise, bouncetime=200)
 GPIO.add_event_detect(23, GPIO.FALLING, callback=stop, bouncetime=200)
 
+# Wait until falling edge
 try:
     GPIO.wait_for_edge(27, GPIO.FALLING)
 except KeyboardInterrupt:
