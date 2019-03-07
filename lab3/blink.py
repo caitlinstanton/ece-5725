07f6c639485ef409804cf13e1d682411322fa3e4
bmq4 <bmq4@cornell.edu>
2019-03-07 16:39:50 -0500

    Start lab3

---
+++ lab3/blink.py
@@ -0,0 +1,18 @@
+# Brandon Quinlan (bmq4) and Caitlin Stanton (cs968)
+# ECE5725, Lab 3, Due 3/21
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO_pin = 13
+freq = 1
+dc = 50.0
+
+p = GPIO.PWM(GPIO_pin, freq)
+p.start(dc)
+
+while GPIO.input(27):
+    pass
+
+p.stop()
