c54f0c2494dedf1558426f1cb54630f5d959361b
Caitlin Stanton <caitlinstanton44@gmail.com>
2019-03-07 18:26:22 -0500

    i deleted the gpio buttons whoops

--- lab3/rolling_control.py
+++ lab3/rolling_control.py
@@ -14,7 +14,15 @@ os.putenv('SDL_MOUSEDEV','/dev/input/touchscreen')
 pygame.init()
 pygame.mouse.set_visible(False)
 WHITE = 255,255,255
+BLACK = 0,0,0
+RED = 255,0,0
+GREEN = 0,255,0
 screen = pygame.display.set_mode((320,240))
+
+panic_resume = "panic stop"
+game_buttons = {'clockwise':(40,180),'counter':(130,180),'stop':(210,180), \
+    'swap':(280,180),panic_resume:(40,80),'quit':(280,80)}
+screen.fill(BLACK)
 
 def set_direction(PWM_number, direction):
     if direction == "stop":
@@ -38,37 +46,44 @@ right_servo = GPIO.PWM(26, 50)
 left_servo.start(0)
 right_servo.start(0)
 
-GPIO.setup(17,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(19,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(27,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 left = False
+code_running = True
 
-def stop(channel):
+def stop():
     print "button 23"
     if left:
         set_direction(left_servo, "stop")
     else:
         set_direction(right_servo, "stop")
-def clockwise(channel):
+def clockwise():
     print "button 22"
     if left:
         set_direction(left_servo, "clockwise")
     else:
         set_direction(right_servo, "clockwise")
-def counter_clockwise(channel):
+def counter_clockwise():
     print "button 17"
     if left:
         set_direction(left_servo, "counter-clockwise")
     else:
         set_direction(right_servo, "counter-clockwise")
-def swap_servo(channel):
+def swap_servo():
     print "button 19"
     global left 
     left = not left
-    
+
+while code_running:    
+    screen.fill(BLACK)
+    for my_text, text_pos in game_buttons.items(): 
+        if my_text == "panic stop":
+            text_surface = my_font.render(my_text,True,RED)
+        else if my_text == "resume":
+            text_surface = my_font.render(my_text,True,GREEN)
+        else:
+            text_surface = my_font.render(my_text,True,WHITE)
+        rect = text_surface.get_rect(center=text_pos)
+        screen.blit(text_surface,rect)
+
 
 GPIO.add_event_detect(17, GPIO.FALLING, callback=counter_clockwise, bouncetime=200)
 GPIO.add_event_detect(19, GPIO.FALLING, callback=swap_servo, bouncetime=200)
