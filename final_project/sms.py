0b6a613a699ec155758ac46203f671ebd1d98db3
Caitlin Stanton <caitlinstanton44@gmail.com>
2019-04-18 17:06:19 -0400

    twilio program

---
+++ final_project/sms.py
@@ -0,0 +1,12 @@
+from twilio.rest import Client
+
+account_sid = 'AC04b78b8dc7118d877990cb3513cfb406'
+auth_token = 'bca894f066779ee1b55a0b52231993ca'
+client = Client(account_sid,auth_token)
+
+message = client.messages.create(
+	body='hello hello',
+	from_='+19179946042',
+	to='+16465521948'
+)
+print(message.sid)
