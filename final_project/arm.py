2e82ada7334668d7260f4a76a9fbcdb334f7a415
Brandon Quinlan <bmq4@cornell.edu>
2019-04-25 19:24:01 -0400

    Incorporated line break sensors into notify.py

--- final_project/arm.py
+++ final_project/arm.py
@@ -1,6 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
+def change_PWM(on_time, p):
+    freq = 1000.0/(20.0+on_time)
+    dc = 100.0*(on_time/(20.0+on_time))
+    p.ChangeFrequency(freq)
+    p.ChangeDutyCycle(dc)
+
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -9,25 +15,18 @@ GPIO.setup(13, GPIO.OUT)
 # Define constants (for stopping)
 GPIO_pin = 13
 
-on_time = 1.35
+on_time = 1.5
 freq = 1000.0/(20.0+on_time)
 dc = 100.0*(on_time/(20.0+on_time))
 
 # Start PWM
 p = GPIO.PWM(GPIO_pin, freq)
 p.start(dc)
-time.sleep(3)
-on_time = 1
-freq = 1000.0/(20.0+on_time)
-dc = 100.0*(on_time/(20.0+on_time))
-p.ChangeFrequency(freq)
-p.ChangeDutyCycle(dc)
-time.sleep(3)
-on_time = 1.35
-freq = 1000.0/(20.0+on_time)
-dc = 100.0*(on_time/(20.0+on_time))
-p.ChangeFrequency(freq)
-p.ChangeDutyCycle(dc)
-time.sleep(3)
+direction = False
+start_time = time.time()
+while time.time() < start_time+10:
+    change_PWM(direction + 1, p)
+    time.sleep(0.3)
+    direction = not direction
 p.stop()
 GPIO.cleanup()
--- final_project/notifs.py
+++ final_project/notifs.py
@@ -3,14 +3,21 @@ import time
 from twilio.rest import Client
 import RPi.GPIO as GPIO
 
+def change_PWM(on_time, p):
+    freq = 1000.0/(20.0+on_time)
+    dc = 100.0*(on_time/(20.0+on_time))
+    p.ChangeFrequency(freq)
+    p.ChangeDutyCycle(dc)
+    
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(13, GPIO.OUT)
+GPIO.setup(5,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Define constants
 GPIO_pin = 13
-on_time = 1.35
+on_time = 1.2
 freq = 1000.0/(20.0+on_time)
 dc = 100.0*(on_time/(20.0+on_time))
 p = GPIO.PWM(GPIO_pin, freq)
@@ -20,35 +27,22 @@ account_sid = 'AC04b78b8dc7118d877990cb3513cfb406'
 auth_token = 'bca894f066779ee1b55a0b52231993ca'
 client = Client(account_sid,auth_token)
 
-food_time = (14,32)
+food_time = (19,23)
 current_time = (datetime.datetime.now().time().hour, datetime.datetime.now().time().minute)
 while (food_time != current_time):
     current_time = (datetime.datetime.now().time().hour, datetime.datetime.now().time().minute)
 print("Time for food!")
 
-on_time = 1
-freq = 1000.0/(20.0+on_time)
-dc = 100.0*(on_time/(20.0+on_time))
-p.ChangeFrequency(freq)
-p.ChangeDutyCycle(dc)
-time.sleep(3)
-on_time = 1.35
-freq = 1000.0/(20.0+on_time)
-dc = 100.0*(on_time/(20.0+on_time))
-p.ChangeFrequency(freq)
-p.ChangeDutyCycle(dc)
+change_PWM(2, p)
+while GPIO.input(5):
+    pass
+change_PWM(1.2, p)
 
 message = client.messages.create(
 	body=current_time,
 	from_='+19179946042',
 	to='+16465521948'
 )
-#print(message.sid)
-
-
-# Continue until quit button pressed
-while GPIO.input(27):
-    pass
 
 p.stop()
 GPIO.cleanup()
