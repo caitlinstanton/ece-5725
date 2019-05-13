96313dec06e3cb706571269496f11ef7e4f35b6d
Caitlin Stanton <caitlinstanton44@gmail.com>
2019-05-13 17:53:31 -0400

    C

--- final_project/client.py
+++ final_project/client.py
@@ -1,11 +1,11 @@
 import socket
 
-s = socket.socket()
+def connect():
+	s = socket.socket()
+	port = 6166
+	s.connect(('10.148.2.162',port))
+	print s.recv(1024)
+	s.close()
 
-port = 6166
-
-s.connect(('10.148.2.162',port))
-
-print s.recv(1024)
-
-s.close()
+if __name__=="__main__":
+   connect()
