feb7fbe2cf377514f51895f266fd6946e92e4a6c
Caitlin Stanton <caitlinstanton44@gmail.com>
2019-05-14 19:54:21 -0400

    C

--- final_project/client.py
+++ final_project/client.py
@@ -4,6 +4,13 @@ s = socket.socket()
 s.bind(('',2019))
 servo = False
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+freq=1000.0/21.5
+p=GPIO.PWM(2,freq)
+dc=100.0*(1.5/21.5)
+p.start(dc)
+
 def waiting():
     s.listen(5)
     c,addr = s.accept()
@@ -13,6 +20,8 @@ def waiting():
 
 def playFetch():
     print "yeet"
+    p.ChangeFrequency(1000.0/21.0)
+    p.ChangeDutyCycle(100.0*(1.0/21.0))
 
 if __name__=="__main__":
     while True:
