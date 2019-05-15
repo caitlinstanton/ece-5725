531177e1acca69dd497c11f78a642bb61ec36400
Brandon Quinlan <bmq4@cornell.edu>
2019-05-15 15:21:16 -0400

    C

--- final_project/hall_sensor.py
+++ final_project/hall_sensor.py
@@ -6,6 +6,7 @@ import socket
 
 import messages
 #import servomotion
+GPIO.setmode(GPIO.BCM)
 
 timeVal = [0,0]
 diameter = 15.24 #centimeters
@@ -17,12 +18,13 @@ GPIO_pin = 13
 on_time = 1.2
 freq = 1000.0/(20.0+on_time)
 dc = 100.0*(on_time/(20.0+on_time))
+GPIO.setup(13,GPIO.OUT)
 p = GPIO.PWM(GPIO_pin, freq)
 p.start(dc)
-food_time = (15,15)
+food_time = (15,21)
 current_time = (datetime.datetime.now().time().hour, datetime.datetime.now().time().minute)
 
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 
 def fetchCallback(channel):
   # Called if sensor output changes
@@ -75,12 +77,14 @@ def main():
   # Get initial reading
   fetchCallback(12)
   petCallback(13)
+  global current_time
 
   while (food_time != current_time):
       current_time = (datetime.datetime.now().time().hour, datetime.datetime.now().time().minute)
   print("Time for food!")
 
   change_PWM(2, p)
+  time.sleep(1)
   while GPIO.input(5):
       pass
   change_PWM(1.2, p)
@@ -99,10 +103,10 @@ def main():
 # Pull high by default
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(12, GPIO.BOTH, callback=fetchCallback, bouncetime=200)
-GPIO.setup(13,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(13,GPIO.BOTH,callback=petCallback,bouncetime=200)
+#GPIO.setup(13,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+#GPIO.add_event_detect(13,GPIO.BOTH,callback=petCallback,bouncetime=200)
 
-GPIO.setup(13, GPIO.OUT)
+#GPIO.setup(13, GPIO.OUT)
 GPIO.setup(5,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 if __name__=="__main__":
