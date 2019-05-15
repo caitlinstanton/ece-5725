66c0b5527aa413d1119dd53ba979f40d9d57a3ab
Caitlin Stanton <caitlinstanton44@gmail.com>
2019-05-14 21:32:32 -0400

    C

--- final_project/messages.py
+++ final_project/messages.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 import socket
 
-account_sid = 'AC04b78b8dc7118d877990cb3513cfb406'
-auth_token = 'bca894f066779ee1b55a0b52231993ca'
+account_sid = 'ACca7bb1631080e074755b158782952729'
+auth_token = '7b033d60148eb46db0b0db6a6e3df06e'
 client = Client(account_sid,auth_token)
 
 def send_sms(text):
 	message = client.messages.create(
 		body=text,
-		from_='+19179946042',
+		from_='+19179050866',
 		to='+16465521948'
 	)
 	print(message.sid)
