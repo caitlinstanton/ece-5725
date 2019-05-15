52b427ac4ae83341c8e910184517cb76e4d42b1c
Caitlin Stanton <caitlinstanton44@gmail.com>
2019-05-15 17:07:03 -0400

    separate files

--- final_project/arm.py
+++ final_project/arm.py
@@ -1,32 +1,51 @@
 import RPi.GPIO as GPIO
 import time
 
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
+on_time = 1.5
+freq = 1000.0/(20.0+on_time)
+dc = 100.0*(on_time/(20.0+on_time))
+pet1 = GPIO.PWM(20, freq)
+pet2 = GPIO.PWM(21,freq)
+pet1.start(dc)
+pet2.start(dc)
+
 def change_PWM(on_time, p):
     freq = 1000.0/(20.0+on_time)
     dc = 100.0*(on_time/(20.0+on_time))
     p.ChangeFrequency(freq)
     p.ChangeDutyCycle(dc)
 
-# Set up GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(13, GPIO.OUT)
+def petCallback(channel):
+  motion = [1.3,1.7]
+  i = 0
+  while not (GPIO.input(channel)):
+      print "Pet in range"
+      change_PWM(pet1,motion[i%2])
+      change_PWM(pet2,motion[i%2])
+      i = i + 1
 
-# Define constants (for stopping)
-GPIO_pin = 13
+def main():
+	petCallback(19)
 
-on_time = 1.5
-freq = 1000.0/(20.0+on_time)
-dc = 100.0*(on_time/(20.0+on_time))
+	try:
+	  # Loop until users quits with CTRL-C
+	  while True :
+	      time.sleep(0.1)
 
-# Start PWM
-p = GPIO.PWM(GPIO_pin, freq)
-p.start(dc)
-direction = False
-start_time = time.time()
-while time.time() < start_time+10:
-    change_PWM(direction + 1, p)
-    time.sleep(0.3)
-    direction = not direction
+	except KeyboardInterrupt:
+	  # Reset GPIO settings
+	  p.stop()
+	  GPIO.cleanup()
+
+GPIO.setup(19,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(19,GPIO.BOTH,callback=petCallback,bouncetime=200)
+
 p.stop()
 GPIO.cleanup()
+
+if __name__=="__main__":
+   main()
--- final_project/main.py
+++ final_project/main.py
@@ -14,6 +14,29 @@ circumference = math.pi*diameter
 numPasses = 0
 velocity = 0 #cm/s
 
+# Food dispenser
+GPIO_pin = 13
+on_time = 1.2
+freq = 1000.0/(20.0+on_time)
+dc = 100.0*(on_time/(20.0+on_time))
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(5,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+food = GPIO.PWM(GPIO_pin, freq)
+food.start(dc)
+food_time = (15,55)
+current_time = (datetime.datetime.now().time().hour, datetime.datetime.now().time().minute)
+
+# Petting
+GPIO.setup(20,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
+on_time = 1.5
+freq = 1000.0/(20.0+on_time)
+dc = 100.0*(on_time/(20.0+on_time))
+pet1 = GPIO.PWM(20, freq)
+pet2 = GPIO.PWM(21,freq)
+pet1.start(dc)
+pet2.start(dc)
+
 def fetchCallback(channel):
   # Called if sensor output changes
   stamp = time.time()
@@ -44,6 +67,23 @@ def calculate(magnetPass):
   global velocity
   velocity = circumference/period
 
+def change_PWM(on_time, p):
+    freq = 1000.0/(20.0+on_time)
+    dc = 100.0*(on_time/(20.0+on_time))
+    p.ChangeFrequency(freq)
+    p.ChangeDutyCycle(dc)
+
+def petCallback(channel):
+  motion = [1.3,1.7]
+  i = 0
+  while not (GPIO.input(channel)):
+      print "Pet in range"
+      change_PWM(pet1,motion[i%2])
+      change_PWM(pet2,motion[i%2])
+      i = i + 1
+
+
+
 def main():
   # Wrap main content in a try block so we can
   # catch the user pressing CTRL-C and run the
@@ -53,7 +93,19 @@ def main():
 
   # Get initial reading
   fetchCallback(12)
-  
+  #petCallback(13)
+  global current_time
+
+  while (food_time != current_time):
+      current_time = (datetime.datetime.now().time().hour, datetime.datetime.now().time().minute)
+  print("Time for food!")
+
+  change_PWM(2.5, food)
+  time.sleep(1)
+  while GPIO.input(5):
+      pass
+  change_PWM(1.2, food)
+
   try:
     # Loop until users quits with CTRL-C
     while True :
@@ -68,6 +120,8 @@ def main():
 # Pull high by default
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(12, GPIO.BOTH, callback=fetchCallback, bouncetime=200)
+#GPIO.setup(13,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+#GPIO.add_event_detect(13,GPIO.BOTH,callback=petCallback,bouncetime=200)
 
 if __name__=="__main__":
    main()
