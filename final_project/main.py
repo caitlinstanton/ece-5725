fb1abb9f3706380329d5c6a68c8f48725ea70668
bmq4 <bmq4@cornell.edu>
2019-05-17 19:59:17 -0400

    Added comments to main

--- final_project/main.py
+++ final_project/main.py
@@ -7,14 +7,14 @@ import messages
 
 GPIO.setmode(GPIO.BCM)
 
-# Fetch
+# Variables to be used for fetch 
 timeVal = [0,0]
 diameter = 15.24 #centimeters
 circumference = math.pi*diameter
 numPasses = 0
 velocity = 0 #cm/s
 
-# Food dispenser
+# Variables to be used for food dispenser
 GPIO_pin = 13
 on_time = 1.2
 freq = 1000.0/(20.0+on_time)
@@ -26,7 +26,7 @@ food.start(dc)
 food_time = (15,55)
 current_time = (datetime.datetime.now().time().hour, datetime.datetime.now().time().minute)
 
-# Petting
+# Variables to be used for petting
 GPIO.setup(20,GPIO.OUT)
 GPIO.setup(21,GPIO.OUT)
 on_time = 1.5
@@ -48,16 +48,17 @@ def fetchCallback(channel):
     print("Sensor LOW")
     calculate(stamp)
     if velocity > 50 or numPasses > 10:
-        print "hall"
         global numPasses
-        #messages.send_sms("Playing fetch!")
-        '''s = socket.socket()
+        messages.send_sms("Playing fetch!")
+        # connect to Pi zero
+        s = socket.socket()
         s.connect(('10.148.12.144',5725))
         print "connected"
         server = s.recv(1024)
-        s.close()'''
+        s.close()
         numPasses = 0
 
+# Update velocity
 def calculate(magnetPass):
   global numPasses
   numPasses = numPasses + 1
@@ -67,12 +68,14 @@ def calculate(magnetPass):
   global velocity
   velocity = circumference/period
 
+# Changes PWM signal of p to be high for "on_time" microseconds
 def change_PWM(on_time, p):
     freq = 1000.0/(20.0+on_time)
     dc = 100.0*(on_time/(20.0+on_time))
     p.ChangeFrequency(freq)
     p.ChangeDutyCycle(dc)
 
+# ISR for petting arm
 def petCallback(channel):
   motion = [1.3,1.7]
   i = 0
@@ -90,20 +93,19 @@ def main():
   # GPIO cleanup function. This will also prevent
   # the user seeing lots of unnecessary error
   # messages.
-
-  # Get initial reading
-  fetchCallback(12)
-  #petCallback(13)
   global current_time
 
+  # Wait until feeding time
   while (food_time != current_time):
       current_time = (datetime.datetime.now().time().hour, datetime.datetime.now().time().minute)
   print("Time for food!")
 
+  # Open tank
   change_PWM(2.5, food)
   time.sleep(1)
   while GPIO.input(5):
       pass
+  # close tank
   change_PWM(1.2, food)
 
   try:
@@ -117,11 +119,11 @@ def main():
     GPIO.cleanup()
 
 # Set Switch GPIO as input
-# Pull high by default
+# Setup ISRs
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(12, GPIO.BOTH, callback=fetchCallback, bouncetime=200)
-#GPIO.setup(13,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-#GPIO.add_event_detect(13,GPIO.BOTH,callback=petCallback,bouncetime=200)
+GPIO.setup(13,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(13,GPIO.BOTH,callback=petCallback,bouncetime=200)
 
 if __name__=="__main__":
    main()
